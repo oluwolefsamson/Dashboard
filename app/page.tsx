3c8c1cd321f5596822580593f42683eb843aa400
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-02-28 11:40:04 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -13,14 +13,12 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        
-          <div className={styles.shape} />;
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Oluwole Samson Olawumi Dashboard.</strong> This design is {' '}
             <a href="https://nextjs.org/learn/" className="text-green-500">
-              Next.js Learn Course
+              
             </a>
-            , brought to you by Samtech.
+              Brought to you by Samtech.
           </p>
           <Link
             href="../dashboard"
@@ -32,18 +30,18 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
+            src="/hero-desktop.jpg"
+            width={700}
+            height={300}
+            className="hidden md:block rounded-lg border border-gray-300 p-4"
             alt="Screenshots of the dashboard project showing desktop version"
           />
 
           <Image
-            src="/hero-mobile.png"
+            src="/hero-mobile.jpg"
             width={560}
             height={620}
-            className="block md:hidden"
+            className="block md:hidden rounded-sm border border-gray-300 p-4"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
